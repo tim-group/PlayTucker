e2bbc2c1478c156a30482d7c72836c7e9167b3a0
Francesco Gigli <francesco.gigli@timgroup.com>
2014-02-05 17:22:11 +0000

    fgigli/jheister: added Info Controller and TuckerReader helper

--- sample/test/IntegrationSpec.scala
+++ sample/test/IntegrationSpec.scala
@@ -1,17 +1,27 @@
-package test
-
+import com.timgroup.tucker.info.Status
+import helper.TuckerReader
 import org.specs2.mutable._
 
 import play.api.test._
 import play.api.test.Helpers._
 
 class IntegrationSpec extends Specification {
-  "Application" should {
-    "returns hello world" in {
-      running(TestServer(3333), HTMLUNIT) { browser =>
-        browser.goTo("http://localhost:3333/")
-        browser.pageSource must contain("Hello World!")
-      }
+  "Status page" should {
+    "contains component for the app version" in {
+      val result = routeAndCall(FakeRequest("GET", "/info/status")).get
+
+      val component = TuckerReader.componentFor(result)("version")
+
+      component.status must be (Status.INFO)
     }
+
+
+//    "contains component for each datasource" in {
+//      val result = routeAndCall(FakeRequest("GET", "/info/status")).get
+//
+//      val component = TuckerReader.componentFor(result)("BoneCp-default")
+//
+//      component.status must be (Status.OK)
+//    }
   }
 }
